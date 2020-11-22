6f59ac553d55112ac7b807bb7b7ded174b94dcc8
hayasan19 <70267022+hayasan19@users.noreply.github.com>
2020-11-22 12:01:53 +0900

    ユーザー管理機能(修正1回目)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-
-validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'は英数混合で入力してください。'}
-validates :nickname, presence: true
-validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ ,message: 'は全角で入力して下さい。'}
-validates :last_name, presence:true, format: {with: /\A[ぁ-んァ-ン一-龥]/ ,message: 'は全角で入力して下さい。'}
-validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-validates :birthday, presence: true
-  
-end
+         with_options presence: true do
+         validates :nickname, presence: true
+         validates :birthday, presence: true
+         end
+         with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'は英数混合で入力してください。'} do
+         validates :password
+         end
+         
+         with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ ,message: 'は全角で入力して下さい。'} do
+         validates :first_name
+         validates :last_name
+         end
+         
+         with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'} do
+          validates :first_name_kana
+          validates :last_name_kana
+          end
+            
+          end
+        
+         
