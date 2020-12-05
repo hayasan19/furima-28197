2d12d9fc4cd97d314632b6ad88bba0f6745de9ff
hayasan19 <70267022+hayasan19@users.noreply.github.com>
2020-12-05 16:48:02 +0900

    商品出品機能（修正２回目）

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,21 +7,12 @@ class Item < ApplicationRecord
   belongs_to :item_fee_status
   belongs_to :item_prefecture
   belongs_to :item_scheduled_delivery
-  
+
   with_options presence: true do
   validates :image
-  end
-
-  with_options presence: true, length: {maximum: 40} do
-  validates :name
-  end
-
-  with_options presence: true, length: {maximum: 1000} do
-  validates :text
-  end
-  
-  with_options presence: true, inclusion: { in: 300..9999999,message: 'Out of setting range' } do
-  validates :price
+  validates :name, length: {maximum: 40}
+  validates :text, length: {maximum: 1000}
+  validates :price, inclusion: { in: 300..9999999,message: 'Out of setting range' }
 end
 
     #カテゴリーの選択が「---」の時は保存できないようにするOut of setting range
