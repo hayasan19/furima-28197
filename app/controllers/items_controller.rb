9012364d259092494af569150f82d59ea0b122f5
hayasan19 <70267022+hayasan19@users.noreply.github.com>
2020-12-20 19:49:40 +0900

    商品情報編集機能(修正１回目)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @items = Item.all.order('id DESC')
   end
@@ -18,8 +19,6 @@ class ItemsController < ApplicationController
     end
   end
 
- 
-
   def edit
     @item = Item.find(params[:id])
   end
@@ -27,8 +26,12 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(items_params)
+    if @item.save
     redirect_to item_path
+    else
+      render "edit"
   end
+end
 
   def show
     @item = Item.find(params[:id])
@@ -38,5 +41,8 @@ private
 def items_params
   params.require(:item).permit(:image, :name,:text,:category_id,:item_status_id,:item_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:price).merge(user_id: current_user.id)
 end
-end
 
+def set_item
+  @item = Item.find(params[:id])
+end
+end
